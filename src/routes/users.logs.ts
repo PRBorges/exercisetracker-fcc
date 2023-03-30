cdf68d092229853b9ecf6202aa01d6b4f3b3ce22
Pedro R. Borges <pedroR.borges@gMail.com>
2023-03-30 22:57:53 +0100

    Use express-validator in log requests.

--- src/routes/users.logs.ts
+++ src/routes/users.logs.ts
@@ -2,7 +2,9 @@
 
 import express from "express";
 import mongoose, { PipelineStage } from "mongoose";
+import { query, validationResult } from "express-validator";
 import MUser from "../models/user";
+import adjustDate from "../adjustDate";
 
 type DateBound =
   | { $gte: [string, Date] }
@@ -11,89 +13,91 @@ type DateBound =
 
 const userLogsRouter = express.Router();
 
-userLogsRouter.get("/:_ID/logs", async (req, res) => {
-  // Validation of query parameters
-  let userID, fromDate, toDate, limit;
-  try {
-    userID = new mongoose.Types.ObjectId(req.params._ID);
-    fromDate = validDateOrNull(req.query.from);
-    toDate = validDateOrNull(req.query.to);
-    limit = validLimitOrNull(req.query.limit);
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(400).json({ error: err.message });
-      console.log("Invalid query parameter: ", err.message);
-    } else {
-      console.error(err);
-    }
-    return;
-  }
-
-  // Build the query
-  const pipeline: PipelineStage[] = [{ $match: { _id: userID } }];
-
-  if (fromDate || toDate) {
-    const dateBounds = bounds(fromDate, toDate);
-    pipeline.push({
-      $project: {
-        log: {
-          $filter: {
-            input: "$log",
-            as: "exercise",
-            cond: dateBounds,
-          },
-        },
-      },
-    });
-  }
-
-  if (limit) {
-    pipeline.push({
-      $project: { log: { $slice: ["$log", limit] } },
-    });
-  }
-
-  // Execute the query
-  try {
-    const docs = await MUser.aggregate(pipeline).exec();
-
-    if (docs.length == 0) {
-      res.status(400).json({ error: "User not found" });
-      console.log(`User with _ID ${userID} was not found in log request`);
+// Gets exercise log for  a user
+userLogsRouter.get(
+  "/:_ID/logs",
+  [
+    query("from")
+      .optional({ checkFalsy: true })
+      .isISO8601({ strict: true })
+      .toDate(),
+    query("to")
+      .optional({ checkFalsy: true })
+      .isISO8601({ strict: true })
+      .toDate(),
+    query("limit").optional({ checkFalsy: true }).isInt({ gt: 0 }).toInt(),
+  ],
+  async (req, res) => {
+    // Validation of query parameters
+    let userID;
+    try {
+      userID = new mongoose.Types.ObjectId(req.params._ID);
+    } catch (err) {
+      res.status(400).json({ error: "Invalid user ID" });
+      console.log("Invalid user ID in log request: ", err);
       return;
     }
 
-    const user = docs[0];
-    // Change date format in log
-    user.log = user.log.map((exercise) => {
-      exercise.date = exercise.date.toDateString();
-      return exercise;
-    });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      console.log("Bad log request: ", errors.array());
+      return;
+    }
+    const { from, to, limit } = req.query;
 
-    res.status(200).json({ ...user, count: user.log.length });
-  } catch (_err) {}
-});
+    // Build the query
+    const pipeline: PipelineStage[] = [{ $match: { _id: userID } }];
 
-function validDateOrNull(date: any): Date | null {
-  if (date === undefined || date === "") {
-    return null;
+    if (from || to) {
+      const dateBounds = bounds(from, to);
+      pipeline.push({
+        $project: {
+          log: {
+            $filter: {
+              input: "$log",
+              as: "exercise",
+              cond: dateBounds,
+            },
+          },
+        },
+      });
+    }
+
+    if (limit) {
+      pipeline.push({
+        $project: { log: { $slice: ["$log", limit] } },
+      });
+    }
+
+    // Execute the query
+    try {
+      const docs = await MUser.aggregate(pipeline).exec();
+
+      if (docs.length == 0) {
+        res.status(400).json({ error: "User not found" });
+        console.log(`User with _ID ${userID} was not found in log request`);
+        return;
+      }
+
+      const user = docs[0];
+      // Change date format in log
+      user.log = user.log.map((exercise) => {
+        exercise.date = exercise.date.toDateString();
+        return exercise;
+      });
+
+      res.status(200).json({ ...user, count: user.log.length });
+    } catch (_err) {}
   }
-  if (typeof date !== "string") {
-    throw new Error("Invalid date");
-  }
-  const result = new Date(date);
-  if (isNaN(result.getTime())) {
-    throw new Error("Invalid date");
-  }
-  return result;
-}
+);
 
 function bounds(fromDate: Date | null, toDate: Date | null): DateBound {
   const fromBound: DateBound | null = fromDate
-    ? { $gte: ["$$exercise.date", fromDate] }
+    ? { $gte: ["$$exercise.date", adjustDate(fromDate)] }
     : null;
   const toBound: DateBound | null = toDate
-    ? { $lte: ["$$exercise.date", toDate] }
+    ? { $lte: ["$$exercise.date", adjustDate(toDate)] }
     : null;
   if (fromBound && toBound) {
     return { $and: [fromBound, toBound] };
@@ -102,17 +106,6 @@ function bounds(fromDate: Date | null, toDate: Date | null): DateBound {
   } else {
     return toBound as DateBound;
   }
-}
-
-function validLimitOrNull(arg: any): number | null {
-  if (arg === undefined || arg === "") {
-    return null;
-  }
-  const limit = Number(arg);
-  if (isNaN(limit) || !Number.isInteger(limit)) {
-    throw new Error("Invalid limit");
-  }
-  return limit;
 }
 
 export default userLogsRouter;
